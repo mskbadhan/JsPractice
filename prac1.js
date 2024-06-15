55318412b895a7cc5e0a7055a684fbbd2daf5782
mskbadhan <msk.badhan@gmail.com>
2024-06-15 11:25:16 +0600

    Personal wallet donation functionality added.

--- prac1.js
+++ prac1.js
@@ -1,13 +1,23 @@
 //Main Description will be found in the README 
 let addMoneyBtn = document.querySelector("#addMoneyBtn");
-let donateTKBtn = document.querySelector("#donateTKBtn");
-let donateMoney = document.getElementById("donateTK").value;
 addMoneyBtn.addEventListener("click", ()=>{
   let mainBalance = document.querySelector(".mainBalance").textContent;
       mainBalance = Number(mainBalance);
-  let addMoney = document.getElementById("addMoney").value;
+      let addMoney = document.getElementById("addMoney").value;
       addMoney = Number(addMoney);
-    ADD = mainBalance+addMoney;
-   let finalBalance = document.querySelector(".mainBalance").textContent = `${ADD}`;
-   finalBalance;
-})
+      ADD = mainBalance+addMoney;
+      let finalBalance = document.querySelector(".mainBalance").textContent = `${ADD}`;
+      finalBalance;
+    })
+    
+    //Now donation functionality
+    let donateTKBtn = document.querySelector("#donateTKBtn");
+donateTKBtn.onclick = function donateFunc(){
+  let donateTK = document.querySelector("#donateTK").value;
+  donateTK = Number (donateTK);
+  let mainBalance = document.querySelector(".mainBalance").textContent;
+  mainBalance = Number(mainBalance);
+  subtract = mainBalance-donateTK;
+  let finalsub = document.querySelector(".mainBalance").textContent = `${subtract}`;
+  finalsub;
+}    
